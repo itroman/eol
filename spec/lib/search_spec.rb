c9fb86f566a6917315b89a5107f97fe2b34a01d2
Robert Roman <rob@romancoding.com>
2017-06-13 08:02:26 +0000

    Additions to search_spec

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -1,24 +1,47 @@
 require 'spec_helper'
 
-describe "A Search object" do
+describe Eolife::Search do
+  
   subject(:search) { Eolife::Search.new( {id: id, title: title, link: link, 
-                    content: content }) }
-  let(:id) {1128718}
-  let(:title) {"Tolumnia bahamensis (Nash) Braem"}
-  let(:link) {"http://eol.org/1128718?action=overview&controller=taxa"}
-  let(:content) {"Tolumnia x adamsii Sauleda"}
+                      content: content }) }
+  
+    context "An empty Search object" do
+    
+      let(:id) {nil}
+      let(:title) {nil}
+      let(:link) {nil}
+      let(:content) {nil}
+  
+      it { is_expected.to be_a(Eolife::Search) }
       
-      it "each Search object has four attributes" do
-        expect(search.id).to be_a(Integer)
-        expect(search.title).to be_a(String)
-        expect(search.link).to be_a(String)
-        expect(search.content).to be_a(String)
+      it "has attributes with nil values" do
+        expect(search.id).to eq(nil)
+        expect(search.title).to eq(nil)
+        expect(search.link).to eq(nil)
+        expect(search.content).to eq(nil)
       end
+      
+  end
+    
+    context "A non-empty Search object" do
+  
+      let(:id) {1128718}
+      let(:title) {"Tolumnia bahamensis (Nash) Braem"}
+      let(:link) {"http://eol.org/1128718?action=overview&controller=taxa"}
+      let(:content) {"Tolumnia x adamsii Sauleda"}
+      
+      it { is_expected.to be_a(Eolife::Search) }
+      
+      
+      it { is_expected.to have_attributes(:id => Integer) }
+      it { is_expected.to have_attributes(:title => String) }
+      it { is_expected.to have_attributes(:link => String) }
+      it { is_expected.to have_attributes(:content => String) }
+      
       
       it "links to a website" do
         expect(search.link).to include "http:"
       end
-        
-    
-  
+  end
+
 end
